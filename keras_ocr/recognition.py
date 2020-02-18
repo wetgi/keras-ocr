619554ee21ddf30d952aa451a0d66ee48e4738fd
Gilles Wetzel <gilles.wetzel@statec.etat.lu>
2020-02-18 10:28:45 +0100

    log ctc

--- keras_ocr/recognition.py
+++ keras_ocr/recognition.py
@@ -165,7 +165,9 @@ def CTCDecoder():
         input_shape = tf.keras.backend.shape(y_pred)
         input_length = tf.ones(shape=input_shape[0]) * tf.keras.backend.cast(
             input_shape[1], 'float32')
-        unpadded = tf.keras.backend.ctc_decode(y_pred, input_length)[0][0]
+        ctc = tf.keras.backend.ctc_decode(y_pred, input_length)
+        print(ctc)
+        unpadded = ctc[0][0]
         unpadded_shape = tf.keras.backend.shape(unpadded)
         padded = tf.pad(unpadded,
                         paddings=[[0, 0], [0, input_shape[1] - unpadded_shape[1]]],
